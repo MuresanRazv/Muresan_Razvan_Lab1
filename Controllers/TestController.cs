638ba322cfd7ad127515190009e61d3e7b3c2555
MuresanRazv <razvy9099@yahoo.com>
2024-11-25 17:57:08 +0200

    partea 2 gata

---
+++ Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Muresan_Razvan_Lab1.Controllers
+{
+    public class TestController : Controller
+    {
+        public String Index()
+        {
+            return "Hello from test controller!";
+        }
+
+        public String Salut()
+        {
+            return "Salutare!";
+        }
+
+        public String SalutParametrizat(String nume, int numar)
+        {
+            return $"Salutare, {nume}! Ai ales numarul: {numar}";
+        }
+    }
+}
